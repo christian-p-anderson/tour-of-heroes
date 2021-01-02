1a3a391fa92bf8c16d8f85ed8da48174d1b2a369
christian-p-anderson <christian.p.anderson1@gmail.com>
2021-01-02 16:14:45 -0500

    added services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,22 +10,32 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
 
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstrom'
   // };
 
-  constructor() {
-  }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero if=${hero.id}`);
   }
-
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
